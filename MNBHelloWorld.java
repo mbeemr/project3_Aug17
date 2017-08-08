3afb950dc175aace050bc747dff2abb2e001a9ff
Me Bee <mnbeemr@gmail.com>
2017-08-08 14:13:21 -0400

    Me Bee adding hello world java

---
+++ MNBHelloWorld.java
@@ -0,0 +1,6 @@
+public class MNBHelloWorld {
+	  public static void main(String[] args) {
+		  // this prints "Hello, World!" to for Lab 5
+		  System.out.println("Hello, World from Maria!");
+	  }
+}
