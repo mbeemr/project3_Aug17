d3c5951e40ff4be85571525702db005ec3d47c88
Me Bee <mnbeemr@gmail.com>
2017-08-08 14:21:26 -0400

    more lines added to java program in develop branch

--- MNBHelloWorld.java
+++ MNBHelloWorld.java
@@ -2,5 +2,6 @@ public class MNBHelloWorld {
 	  public static void main(String[] args) {
 		  // this prints "Hello, World!" to for Lab 5 in develop branch
 		  System.out.println("Hello, World from Maria!n develop branch");
+		  System.out.println("and another line is added");
 	  }
 }
