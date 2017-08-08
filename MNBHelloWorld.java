3bae6cba4c0d65849b37bef156444e0db7ebc192
Me Bee <mnbeemr@gmail.com>
2017-08-08 14:18:15 -0400

    updated java program to reflect I'min develop branch

--- MNBHelloWorld.java
+++ MNBHelloWorld.java
@@ -1,6 +1,6 @@
 public class MNBHelloWorld {
 	  public static void main(String[] args) {
-		  // this prints "Hello, World!" to for Lab 5
-		  System.out.println("Hello, World from Maria!");
+		  // this prints "Hello, World!" to for Lab 5 in develop branch
+		  System.out.println("Hello, World from Maria!n develop branch");
 	  }
 }
